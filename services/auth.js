aee6427dff5a88c5edbeb026b11b0b3106d777c9
Gavin Samuels <gavin.samuels10@gmail.com>
2018-06-23 02:26:45 -0700

    added register function in auth service for new users

--- services/auth.js
+++ services/auth.js
@@ -3,6 +3,41 @@ const fs = require('fs')
 const passport = require('passport')
 const config = require('../config/constants')
 const User = require('../models/User')
+module.exports.register = (body, cb) => {
+  const username = body.username;
+  const password = body.password;
+
+  if (!username || !password) {
+    return cb(null, { status: 422, error: 'You must provide an username and a password' })
+  }
+
+
+  User.findOne({ username: username }, ['username', 'id'], (err, existingUser) => {
+    if (err) {
+      return cb(null, { status: 500, error: 'Cannot retrieve user' });
+    }
+
+    if (existingUser) {
+      return cb(null, { status: 409, error: 'Username already exists' });
+    }
+
+    const newUser = new User({
+      username: username,
+      password: password
+    });
+
+    newUser.save(err => {
+      if (err) {
+        return cb(null, { status: 500, error: 'Cannot save user' });
+      }
+      else {
+        return cb({ email: newUser.email, success: true, status:200 }, null)
+      }
+
+    });
+  });
+}
+
 
 //should return a token for a valid user based on body
 module.exports.login = (req, cb) => {
